26a200be14a06b0bb2644d5d9ff117f0720d7339
honeok <yihaohey@gmail.com>
2024-11-29 16:44:03 +0800

    update

--- interesting/ts.sh
+++ interesting/ts.sh
@@ -1,82 +1,66 @@
-#!/bin/bash
-# Copyright (c) 2024 honeok
-# Current Author: honeok
+#!/usr/bin/env bash
+#
+# Description: TeamSpeak installation script
+#
+# Copyright (C) 2023 - 2024 honeok <yihaohey@gmail.com>
 # Blog: https://www.honeok.com
+# https://github.com/honeok/cross/blob/master/interesting/ts.sh
 
-# Globle Colour
-yellow='\e[33m'
-green='\e[92m'
-red='\e[31m'
-none='\e[0m'
-_yellow() { echo -e ${yellow}$@${none}; }
-_green() { echo -e ${green}$@${none}; }
-_red() { echo -e ${red}$@${none}; }
+yellow='\033[1;33m'
+red='\033[1;31m'
+green='\033[1;32m'
+white='\033[0m'
+_yellow() { echo -e "${yellow}$@${white}"; }
+_red() { echo -e "${red}$@${white}"; }
+_green() { echo -e ${green}$@${white}; }
 
 # Globle Install Path.
-TS_WORKDIR="/data/docker_data/teamspeak"
-COUNTRY=""
+ts_workdir="/data/docker_data/teamspeak"
 
 # Country Select
-if [[ "$(curl -s --connect-timeout 5 ipinfo.io/country)" == "CN" ]]; then
-	COUNTRY="CN"
+if [[ "$(curl -fskSL --connect-timeout 5 ipinfo.io/country)" == "CN" ]]; then
+    country="CN"
 else
-	COUNTRY=""
+    country=""
 fi
 
 # Github Proxy.
 set_region_config() {
-	if [[ "${COUNTRY}" == "CN" ]]; then
-		local github_proxies=("gh-proxy.com" "gh.kejilion.pro" "github.moeyy.xyz")
-		local best_proxy=""
-		local best_time=9999
-		local ping_time=""
-
-		for proxy in "${github_proxies[@]}"; do
-			ping_time=$(ping -c 2 -q "$proxy" | awk -F '/' 'END {print ($5 ? $5 : 9999)}')
-
-			if (( $(echo "$ping_time" | awk '{print int($1+0.5)}') < $best_time )); then
-				best_time=$(echo "$ping_time" | awk '{print int($1+0.5)}')
-				best_proxy=$proxy
-			fi
-		done
-
-		github_proxy="https://$best_proxy/"
-	else
-		github_proxy=""
-	fi
+    if [[ "${country}" == "CN" ]]; then
+        github_proxy="https://gh-proxy.com/"
+    else
+        github_proxy=""
+    fi
 }
 
 # Use Region function set best github proxy.
 set_region_config
 
 ########################################
-# MAIN
 # Check TeamSpeak Container.
 if docker ps --format '{{.Image}}' | grep -q "teamspeak"; then
-	_red "TeamSpeak容器正在运行，请不要重复安装。"
-	exit 0
+    _red "TeamSpeak容器正在运行，请不要重复安装。"
+    exit 0
 fi
 
 # Check Docker Install.
 if ! command -v docker >/dev/null 2>&1; then
-	if [[ "${COUNTRY}" == "CN" ]]; then
-		cd ~
-		curl -fsSL -o "get-docker.sh" "${github_proxy}raw.githubusercontent.com/honeok/shell/main/docker/get-docker-official.sh" && chmod +x get-docker.sh
-		sh get-docker.sh --mirror Aliyun
-		rm -f get-docker.sh
-	else
-		curl -fsSL https://get.docker.com | sh
-	fi
-else
-	:
+    if [[ "${country}" == "CN" ]]; then
+        cd ~
+        curl -fskSL "${github_proxy}raw.githubusercontent.com/honeok/Tools/main/docker/install.sh" -o get-docker.sh && chmod +x get-docker.sh
+        sh get-docker.sh --mirror Aliyun
+        rm -f get-docker.sh
+    else
+        curl -fskSL https://get.docker.com | sh
+    fi
 fi
 
-# Change Dir TS_WORKDIR
-mkdir -p "${TS_WORKDIR}" && cd "${TS_WORKDIR}"
-curl -fsSL -o "docker-compose.yml" "${github_proxy}raw.githubusercontent.com/honeok/conf/main/dockerapp/ts-docker-compose.yml"
-if [[ "${COUNTRY}" == "CN" ]]; then
-	sed -i 's|image: teamspeak:3.13.7|image: registry.cn-chengdu.aliyuncs.com/honeok/teamspeak:3.13.7|' docker-compose.yml
-	sed -i 's|image: mariadb:11.4.2|image: registry.cn-chengdu.aliyuncs.com/honeok/mariadb:11.4.2|' docker-compose.yml
+# Change Dir ts_workdir
+mkdir -p "${ts_workdir}" && cd "${ts_workdir}"
+curl -fskSL "${github_proxy}raw.githubusercontent.com/honeok/Tools/main/dockerapplication/ts-docker-compose.yml" -o docker-compose.yml
+if [[ "${country}" == "CN" ]]; then
+    sed -i 's|image: teamspeak:3.13.7|image: registry.cn-chengdu.aliyuncs.com/honeok/teamspeak:3.13.7|' docker-compose.yml
+    sed -i 's|image: mariadb:11.4.2|image: registry.cn-chengdu.aliyuncs.com/honeok/mariadb:11.4.2|' docker-compose.yml
 fi
 
 # Run TeamSpeak Server
@@ -84,3 +68,5 @@ docker compose up -d
 _yellow "您的TeamSpeak服务器搭建完毕！请在云服务器防火墙放行9987/UDP、10011/TCP、30033/TCP端口。"
 sleep 1s && _yellow "Bey！"
 docker compose ps
+echo "======================================================="
+docker compose logs
