e0c0457f993521782264b99013e7384b38423385
honeok <yihaohey@gmail.com>
2024-11-29 23:57:34 +0800

    update

--- nezha.sh
+++ nezha.sh
@@ -12,7 +12,7 @@ NZ_DASHBOARD_PATH="${NZ_BASE_PATH}/dashboard"
 NZ_AGENT_PATH="${NZ_BASE_PATH}/agent"
 NZ_DASHBOARD_SERVICE="/etc/systemd/system/nezha-dashboard.service"
 NZ_DASHBOARD_SERVICERC="/etc/init.d/nezha-dashboard"
-NZ_VERSION="v0.20.3"
+NZ_VERSION="v0"
 
 red='\033[0;31m'
 green='\033[0;32m'
@@ -366,20 +366,19 @@ install_agent() {
 
     echo "正在获取监控Agent版本号"
 
+    _version=$(curl -sL "https://api.github.com/repos/nezhahq/agent/releases/tags/v0.20.5" | grep "tag_name" | head -n 1 | awk -F ":" '{print $2}' | sed 's/\"//g;s/,//g;s/ //g')
+    if [ -z "$_version" ]; then
+        _version=$(curl -m 10 -sL "https://gitee.com/api/v5/repos/naibahq/agent/tags" | grep -o '"name": *"v0.20.5"' | awk -F '"' '{print $4}')
+    fi
+    if [ -z "$_version" ]; then
+        _version=$(curl -m 10 -sL "https://fastly.jsdelivr.net/gh/nezhahq/agent@v0.20.5/" | grep "option\.value" | awk -F "'" '{print $2}' | sed 's/nezhahq\/agent@/v/g')
+    fi
+    if [ -z "$_version" ]; then
+        _version=$(curl -m 10 -sL "https://gcore.jsdelivr.net/gh/nezhahq/agent@v0.20.5/" | grep "option\.value" | awk -F "'" '{print $2}' | sed 's/nezhahq\/agent@/v/g')
+    fi
 
-    _version=$(curl -m 10 -sL "https://api.github.com/repos/nezhahq/agent/releases/latest" | grep "tag_name" | head -n 1 | awk -F ":" '{print $2}' | sed 's/\"//g;s/,//g;s/ //g')
     if [ -z "$_version" ]; then
-        _version=$(curl -m 10 -sL "https://gitee.com/api/v5/repos/naibahq/agent/releases/latest" | awk -F '"' '{for(i=1;i<=NF;i++){if($i=="tag_name"){print $(i+2)}}}')
-    fi
-    if [ -z "$_version" ]; then
-        _version=$(curl -m 10 -sL "https://fastly.jsdelivr.net/gh/nezhahq/agent/" | grep "option\.value" | awk -F "'" '{print $2}' | sed 's/nezhahq\/agent@/v/g')
-    fi
-    if [ -z "$_version" ]; then
-        _version=$(curl -m 10 -sL "https://gcore.jsdelivr.net/gh/nezhahq/agent/" | grep "option\.value" | awk -F "'" '{print $2}' | sed 's/nezhahq\/agent@/v/g')
-    fi
-
-    if [ -z "$_version" ]; then
-        err "获取版本号失败，请检查本机能否链接 https://api.github.com/repos/nezhahq/agent/releases/latest"
+        err "获取版本号失败，请检查本机能否链接 https://api.github.com/repos/nezhahq/agent/releases/tags/v0.20.5"
         return 1
     else
         echo "当前最新版本为: ${_version}"
