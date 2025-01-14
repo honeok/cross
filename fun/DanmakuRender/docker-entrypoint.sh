e6ce67a424ee016a05fcaa8e905958b0a657004e
honeok <yihaohey@gmail.com>
2025-01-14 17:30:31 +0800

    update

--- fun/DanmakuRender/docker-entrypoint.sh
+++ fun/DanmakuRender/docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [ "$#" -eq 0 ]; then
-    exec python3 -u /DanmakuRender/main.py
+    exec python3 -u main.py
 else
-    exec python3 -u /DanmakuRender/main.py "$@"
+    exec python3 -u main.py "$@"
 fi
