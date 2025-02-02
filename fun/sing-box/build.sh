7dbbadd171f74786cd849b41e4fd6095e8f72825
honeok <yihaohey@gmail.com>
2025-02-02 17:48:00 +0800

    feat: add multi-architecture support and error handling

--- fun/sing-box/build.sh
+++ fun/sing-box/build.sh
@@ -15,25 +15,33 @@
 # You should have received a copy of the GNU General Public License along with
 # this program. If not, see <https://www.gnu.org/licenses/>.
 
-version=$(curl -fsL "https://api.github.com/repos/SagerNet/sing-box/releases/latest" | awk -F '["v]' '/tag_name/{print $5}')
+set \
+    -o errexit \
+    -o nounset \
+    -o xtrace
+
+os_name=$(grep "^ID=" /etc/*release | awk -F'=' '{print $2}' | sed 's/"//g')
+readonly os_name
+
+[ "$os_name" != "alpine" ] && echo "This script is only for Alpine Linux." && exit 1
+
+sb_version=$(curl -fsL "https://api.github.com/repos/SagerNet/sing-box/releases/latest" | awk -F '["v]' '/tag_name/{print $5}')
+readonly sb_version
 
 case "$(uname -m)" in
-    i386)
+    'x86' | 'i686' | 'i386')
         architecture='386'
         ;;
-    x86_64)
+    'x86_64')
         architecture='amd64'
         ;;
-    arm64|aarch64)
+    'aarch64' | 'arm64')
         architecture='arm64'
         ;;
-    armv6)
-        architecture='armv6'
-        ;;
-    armv7)
+    'armv7l')
         architecture='armv7'
         ;;
-    s390x)
+    's390x')
         architecture='s390x'
         ;;
     *)
@@ -42,15 +50,15 @@ case "$(uname -m)" in
 esac
 
 [ -z "$architecture" ] && echo "ERROR: Not supported OS Architecture!" && exit 1
-[ -z "$version" ] && echo "ERROR: Unable to obtain Sing box version!" && exit 1
+[ -z "$sb_version" ] && echo "ERROR: Unable to obtain Sing box version!" && exit 1
 
-if ! curl -fsL -O "https://github.com/SagerNet/sing-box/releases/download/v${version}/sing-box-${version}-linux-${architecture}.tar.gz" >/dev/null 2>&1; then
+if ! curl -fsL -O "https://github.com/SagerNet/sing-box/releases/download/v${sb_version}/sing-box-${sb_version}-linux-${architecture}.tar.gz" >/dev/null 2>&1; then
     echo "ERROR: Download single box failed, please check the network!"
     exit 1
 fi
 
-tar zxf "sing-box-${version}-linux-${architecture}.tar.gz" --strip-components=1
-rm -fv "sing-box-${version}-linux-${architecture}.tar.gz" "LICENSE"
+tar zxf "sing-box-${sb_version}-linux-${architecture}.tar.gz" --strip-components=1
+rm -fv "sing-box-${sb_version}-linux-${architecture}.tar.gz" "LICENSE"
 mv -f "sing-box" "/usr/local/bin/sing-box"
 
 if [ ! -x "/usr/local/bin/sing-box" ]; then
