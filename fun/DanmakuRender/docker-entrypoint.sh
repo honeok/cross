41c7ed0861c32a70bda5bf232259246f0b396e35
honeok <yihaohey@gmail.com>
2025-02-12 17:19:57 +0800

    fix: optimize config directory check logic

--- fun/DanmakuRender/docker-entrypoint.sh
+++ fun/DanmakuRender/docker-entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 work_dir="/DanmakuRender"
-danmaku_config_dir="/DanmakuRender/configs"
-danmaku_config_dir_temp="/DanmakuRender/configs_temp"
+danmaku_config_dir="$work_dir/configs"
+danmaku_config_dir_temp="$work_dir/configs_temp"
 
-if [ -d "$danmaku_config_dir" ] && [ -z "$(ls -A "$danmaku_config_dir" 2>/dev/null)" ]; then
+if [ -d "$danmaku_config_dir" ] && [ -z "$(find "$danmaku_config_dir" -mindepth 1 -print -quit)" ]; then
     cp -rf "$danmaku_config_dir_temp"/* "$danmaku_config_dir/"
 fi
 
