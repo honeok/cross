9f2d9f6150e1f0ab459699749fdc424764745db0
honeok <yihaohey@gmail.com>
2025-02-13 18:25:28 +0800

    merge: sing-box from dev to master

---
+++ fun/sing-box/build.sh
@@ -0,0 +1,68 @@
+#!/bin/sh
+#
+# Description: This script is used to build Sing-box binary files for multiple architectures.
+#
+# Copyright (C) 2025 honeok <honeok@duck.com>
+#
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License, version 3 or later.
+#
+# This program is distributed WITHOUT ANY WARRANTY; without even the implied
+# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# See the LICENSE file or <https://www.gnu.org/licenses/> for full license terms.
+
+set \
+    -o errexit \
+    -o nounset
+
+# Run default path
+SINGBOX_WORKDIR="/etc/sing-box"
+SINGBOX_BINDIR="$SINGBOX_WORKDIR/bin"
+SINGBOX_CONFDIR="$SINGBOX_WORKDIR/conf"
+SINGBOX_LOGDIR="/var/log/sing-box"
+SINGBOX_LOGFILE="$SINGBOX_LOGDIR/access.log"
+
+LATEST_VERSION=$(curl -fskL "https://api.github.com/repos/SagerNet/sing-box/releases/latest" | awk -F '["v]' '/tag_name/{print $5}')
+readonly LATEST_VERSION
+[ -z "$LATEST_VERSION" ] && { echo "ERROR: Unable to obtain Sing-box version!"; exit 1; }
+
+# Determine system arch based
+case "$(uname -m)" in
+    'x86' | 'i686' | 'i386')
+        ARCH='386'   # 32-bit x86
+    ;;
+    'x86_64')
+        ARCH='amd64' # 64-bit x86
+    ;;
+    'aarch64' | 'arm64')
+        ARCH='arm64' # 64-bit ARM
+    ;;
+    'armv7l')
+        ARCH='armv7' # 32-bit ARM
+    ;;
+    's390x')
+        ARCH='s390x' # IBM S390x
+    ;;
+    *)
+        ARCH=''
+    ;;
+esac
+[ -z "$ARCH" ] && { echo "ERROR: Not supported OS ARCH!"; exit 1; }
+
+# Create necessary directories
+mkdir -p "$SINGBOX_WORKDIR" "$SINGBOX_BINDIR" "$SINGBOX_CONFDIR" "$SINGBOX_LOGDIR" 1>/dev/null
+touch "$SINGBOX_LOGFILE" 1>/dev/null
+
+cd "$SINGBOX_BINDIR" ||  { echo "ERROR: Failed to enter the sing-box bin directory!" ; exit 1;}
+
+# Extract and install Sing-Box
+if ! curl -fskL -O "https://github.com/SagerNet/sing-box/releases/download/v${LATEST_VERSION}/sing-box-${LATEST_VERSION}-linux-${ARCH}.tar.gz" ; then
+    echo "ERROR: Download sing-Box failed, please check the network!" && exit 1
+fi
+tar zxf "sing-box-${LATEST_VERSION}-linux-${ARCH}.tar.gz" --strip-components=1 || { echo "ERROR: tar Sing-box package failed!"; exit 1; }
+rm -f "sing-box-${LATEST_VERSION}-linux-${ARCH}.tar.gz" "LICENSE"
+if [ ! -x "$SINGBOX_BINDIR/sing-box" ]; then
+    chmod +x "$SINGBOX_BINDIR/sing-box"
+fi
---
+++ fun/sing-box/docker-entrypoint.sh
@@ -0,0 +1,123 @@
+#!/bin/sh
+#
+# Copyright (C) 2025 honeok <honeok@duck.com>
+#
+# References:
+# https://github.com/233boy/sing-box
+# https://github.com/fscarmen/sing-box
+# https://github.com/RayWangQvQ/sing-box-installer/blob/main/DIY.md
+#
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License, version 3 or later.
+#
+# This program is distributed WITHOUT ANY WARRANTY; without even the implied
+# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# See the LICENSE file or <https://www.gnu.org/licenses/> for full license terms.
+
+set \
+    -o errexit \
+    -o nounset \
+    -o noclobber
+
+SINGBOX_WORKDIR="/etc/sing-box"
+SINGBOX_BINDIR="$SINGBOX_WORKDIR/bin"
+SINGBOX_CMD="$SINGBOX_BINDIR/sing-box"
+SINGBOX_CONFDIR="$SINGBOX_WORKDIR/conf"
+SINGBOX_LOGDIR="/var/log/sing-box"
+SINGBOX_LOGFILE="$SINGBOX_LOGDIR/access.log"
+
+SERVERS="aws.amazon.com www.ebay.com icloud.cdn-apple.com addons.mozilla.org"
+GENERATE_UUID=$(cat /proc/sys/kernel/random/uuid)
+PRIVATE_KEY=$(head -c 32 /dev/urandom | base64 | tr '+/' '-_' | tr -d '=')
+PUBLIC_KEY=$(head -c 32 /dev/urandom | base64 | tr '+/' '-_' | tr -d '=')
+IPADDRESS=$(curl -fsL -m 3 https://ipinfo.io/ip)
+
+# Generate default config if not provided by the user
+if [ ! -f "$SINGBOX_WORKDIR/config.json" ]; then
+    cat > "$SINGBOX_WORKDIR/config.json" <<EOF
+{
+  "log": {
+    "output": "${SINGBOX_LOGFILE}",
+    "level": "info",
+    "timestamp": true
+  },
+  "dns": {},
+  "ntp": {
+    "enabled": true,
+    "server": "time.apple.com"
+  },
+  "outbounds": [
+    {
+      "tag": "direct",
+      "type": "direct"
+    },
+    {
+      "tag": "block",
+      "type": "block"
+    }
+  ]
+}
+EOF
+fi
+
+if [ -d "$SINGBOX_CONFDIR" ] && [ -z "$(ls -A "$SINGBOX_CONFDIR" 2>/dev/null)" ]; then
+    TLS_SERVER=$(echo "$SERVERS" | tr " " "\n" | shuf -n 1)
+    cat > "$SINGBOX_CONFDIR/VLESS-REALITY-30000.json" <<EOF
+{
+  "inbounds": [
+    {
+      "tag": "VLESS-REALITY-30000.json",
+      "type": "vless",
+      "listen": "::",
+      "listen_port": 30000,
+      "users": [
+        {
+          "flow": "xtls-rprx-vision",
+          "uuid": "${GENERATE_UUID}"
+        }
+      ],
+      "tls": {
+        "enabled": true,
+        "server_name": "${TLS_SERVER}",
+        "reality": {
+          "enabled": true,
+          "handshake": {
+            "server": "${TLS_SERVER}",
+            "server_port": 443
+          },
+          "private_key": "${PRIVATE_KEY}",
+          "short_id": [
+            ""
+          ]
+        }
+      }
+    }
+  ],
+  "outbounds": [
+    {
+      "type": "direct"
+    },
+    {
+      "tag": "public_key_${PUBLIC_KEY}",
+      "type": "direct"
+    }
+  ]
+}
+EOF
+
+    {
+        echo "#################### URL ####################"
+        echo ""
+        echo "vless://${GENERATE_UUID}@${IPADDRESS}:30000?encryption=none&security=reality&flow=xtls-rprx-vision&type=tcp&sni=music.apple.com&pbk=${PUBLIC_KEY}&fp=chrome#reality-${PUBLIC_KEY}"
+        echo ""
+        echo "#################### END ####################"
+    } >> "$SINGBOX_LOGFILE"
+fi
+
+if [ "$#" -eq 0 ]; then
+    exec "$SINGBOX_CMD" run -c "$SINGBOX_WORKDIR/config.json" -C "$SINGBOX_CONFDIR"
+else
+    exec "$@"
+fi
