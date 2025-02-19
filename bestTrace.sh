b383c283d6ebeb6c298f16fb2e7b31d6de868e8c
honeok <yihaohey@gmail.com>
2025-02-20 04:55:34 +0800

    chore: simplify comment

--- bestTrace.sh
+++ bestTrace.sh
@@ -4,22 +4,13 @@
 #
 # Copyright (C) 2024 - 2025 honeok <honeok@duck.com>
 #
-# https://github.com/honeok/cross/raw/master/bestTrace.sh
-#
 # This script utilizes NextTrace, a powerful network diagnostic tool.
 # NextTrace is copyrighted and developed by the NextTrace project team.
 # For more details about NextTrace, visit: https://github.com/nxtrace
 #
-# License Information:
-# This program is free software: you can redistribute it and/or modify it under
-# the terms of the GNU General Public License, version 3 or later.
-#
-# This program is distributed WITHOUT ANY WARRANTY; without even the implied
-# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-# General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License along with
-# this program. If not, see <https://www.gnu.org/licenses/>.
+# Licensed under the GNU General Public License, version 2 only.
+# This program is distributed WITHOUT ANY WARRANTY.
+# See <https://www.gnu.org/licenses/old-licenses/gpl-2.0.html>.
 
 # shellcheck disable=SC2034,SC2207
 
