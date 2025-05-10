e878a6f52e0a28b44b18dd15c0f326bb3d614980
honeok <yihaohey@gmail.com>
2025-05-11 02:05:04 +0800

    chore: optimize variable placement and remove redundant parameters

--- fun/sing-box/docker-entrypoint.sh
+++ fun/sing-box/docker-entrypoint.sh
@@ -6,7 +6,7 @@
 # https://sing-box.sagernet.org/zh/configuration
 # https://github.com/233boy/sing-box
 # https://github.com/fscarmen/sing-box
-# https://github.com/RayWangQvQ/sing-box-installer/blob/main/DIY.md
+# https://github.com/RayWangQvQ/sing-box-installer
 #
 # Licensed under the GNU General Public License, version 2 only.
 # This program is distributed WITHOUT ANY WARRANTY.
@@ -21,22 +21,15 @@ SINGBOX_BINDIR="$SINGBOX_WORKDIR/bin"
 SINGBOX_CONFDIR="$SINGBOX_WORKDIR/conf"
 SINGBOX_LOGDIR="/var/log/sing-box"
 SINGBOX_LOGFILE="$SINGBOX_LOGDIR/access.log"
-
-# https://github.com/XTLS/Xray-core/issues/2005
-TLS_SERVERS="www.icloud.com apps.apple.com music.apple.com icloud.cdn-apple.com updates.cdn-apple.com"
-GENERATE_UUID=$(sing-box generate uuid)
-GENERATE_KEYS=$(sing-box generate reality-keypair)
-PRIVATE_KEY=$(printf "%s" "$GENERATE_KEYS" | sed -n 's/^PrivateKey: *\(.*\)$/\1/p')
-PUBLIC_KEY=$(printf "%s" "$GENERATE_KEYS" | sed -n 's/^PublicKey: *\(.*\)$/\1/p')
-# tls generation
-GENERATE_TLS_KEY=$(sing-box generate tls-keypair tls -m 456)
-TLS_PRIVATE_KEY=$(printf "%s\n" "$GENERATE_TLS_KEY" | awk '/-----BEGIN PRIVATE KEY-----/{p=1} p{print} /-----END PRIVATE KEY-----/{p=0;exit}')
-TLS_CERTIFICATE=$(printf "%s\n" "$GENERATE_TLS_KEY" | awk '/-----BEGIN CERTIFICATE-----/{p=1} p{print} /-----END CERTIFICATE-----/{p=0;exit}')
-
-PUBLIC_IP=$(curl -fskL -m 3 -4 https://www.qualcomm.cn/cdn-cgi/trace 2>/dev/null | grep -i '^ip=' | cut -d'=' -f2 | xargs || \
-            curl -fskL -m 3 -6 https://www.qualcomm.cn/cdn-cgi/trace 2>/dev/null | grep -i '^ip=' | cut -d'=' -f2 | xargs)
+CLOUDFLARE_API="www.qualcomm.cn"
+PUBLIC_IP=$(curl -fsL -m 5 -4 "http://$CLOUDFLARE_API/cdn-cgi/trace" 2>/dev/null | awk -F'=' '/^ip=/ {print $2}' || \
+            curl -fsL -m 5 -6 "http://$CLOUDFLARE_API/cdn-cgi/trace" 2>/dev/null | awk -F'=' '/^ip=/ {print $2}')
 
 if [ ! -s "$SINGBOX_BINDIR/tls.key" ] || [ ! -s "$SINGBOX_BINDIR/tls.cer" ]; then
+    # tls generation
+    GENERATE_TLS_KEY=$(sing-box generate tls-keypair tls -m 456)
+    TLS_PRIVATE_KEY=$(printf "%s\n" "$GENERATE_TLS_KEY" | awk '/-----BEGIN PRIVATE KEY-----/{p=1} p{print} /-----END PRIVATE KEY-----/{p=0;exit}')
+    TLS_CERTIFICATE=$(printf "%s\n" "$GENERATE_TLS_KEY" | awk '/-----BEGIN CERTIFICATE-----/{p=1} p{print} /-----END CERTIFICATE-----/{p=0;exit}')
     echo "$TLS_PRIVATE_KEY" > "$SINGBOX_BINDIR/tls.key"
     echo "$TLS_CERTIFICATE" > "$SINGBOX_BINDIR/tls.cer"
 fi
@@ -66,7 +59,13 @@ EOF
 fi
 
 if [ -d "$SINGBOX_CONFDIR" ] && [ -z "$(ls -A "$SINGBOX_CONFDIR" 2>/dev/null)" ]; then
-    TLS_SERVER=$(echo "$TLS_SERVERS" | tr " " "\n" | shuf -n 1)
+    # https://github.com/XTLS/Xray-core/issues/2005
+    TLS_SERVERS="www.icloud.com apps.apple.com music.apple.com icloud.cdn-apple.com updates.cdn-apple.com"
+    GENERATE_UUID=$(sing-box generate uuid || cat /proc/sys/kernel/random/uuid)
+    GENERATE_KEYS=$(sing-box generate reality-keypair)
+    PRIVATE_KEY=$(printf "%s" "$GENERATE_KEYS" | sed -n 's/^PrivateKey: *\(.*\)$/\1/p')
+    PUBLIC_KEY=$(printf "%s" "$GENERATE_KEYS" | sed -n 's/^PublicKey: *\(.*\)$/\1/p')
+    TLS_SERVER=$(printf "%s" "$TLS_SERVERS" | tr " " "\n" | shuf -n 1)
     cat > "$SINGBOX_CONFDIR/VLESS-REALITY-30000.json" <<EOF
 {
   "inbounds": [
@@ -110,15 +109,11 @@ if [ -d "$SINGBOX_CONFDIR" ] && [ -z "$(ls -A "$SINGBOX_CONFDIR" 2>/dev/null)" ]
 }
 EOF
 
-    if [ -z "$PUBLIC_IP" ]; then
-        echo "Error: Failed to retrieve IP address, configuration generation aborted!"; exit 1
-    fi
+    [ -z "$PUBLIC_IP" ] && { printf 'Error: Failed to retrieve IP address, configuration generation aborted!\n'; exit 1; }
 
     {
         echo "-------------------- URL --------------------"
-        echo ""
         echo "vless://${GENERATE_UUID}@${PUBLIC_IP}:30000?encryption=none&security=reality&flow=xtls-rprx-vision&type=tcp&sni=${TLS_SERVER}&pbk=${PUBLIC_KEY}&fp=chrome#REALITY-${PUBLIC_IP}"
-        echo ""
         echo "-------------------- END --------------------"
     } >> "$SINGBOX_LOGFILE"
 fi
