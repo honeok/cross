dcf36aa332f23cbd6a63360ed3d48e2b387d08cd
honeok <yihaohey@gmail.com>
2025-05-15 19:17:37 +0800

    chore: github api version acquisition added retry

--- fun/3x-ui/build.sh
+++ fun/3x-ui/build.sh
@@ -8,14 +8,11 @@
 # This program is distributed WITHOUT ANY WARRANTY.
 # See <https://www.gnu.org/licenses/old-licenses/gpl-2.0.html>.
 
-set \
-    -o errexit \
-    -o nounset \
-    -o xtrace
+set -eux
 
 command -v curl >/dev/null 2>&1 || apk add --no-cache curl
 
-XRAY_VERSION=$(curl -fsL "https://api.github.com/repos/XTLS/Xray-core/releases/latest" | awk -F '["v]' '/tag_name/{print $5}')
+XRAY_VERSION=$(curl -fsL --retry 5 "https://api.github.com/repos/XTLS/Xray-core/releases/latest" | awk -F '["v]' '/tag_name/{print $5}')
 readonly XRAY_VERSION
 [ -z "$XRAY_VERSION" ] && { printf 'Error: Unable to obtain xray version!\n' >&2; exit 1; }
 
