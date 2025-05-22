1001d6f07ccaad9ec15ec9cfd154720ea187e992
honeok <yihaohey@gmail.com>
2025-05-22 23:25:36 +0800

    refactor: modify the run order and define local variables

--- backTrace.sh
+++ backTrace.sh
@@ -11,73 +11,95 @@
 # This program is distributed WITHOUT ANY WARRANTY.
 # See <https://www.gnu.org/licenses/old-licenses/gpl-2.0.html>.
 
-red='\033[91m'
-cyan='\033[96m'
-white='\033[0m'
-_red() { printf "$red%s$white" "$*"; }
-_cyan() { printf "$cyan%s$white" "$*"; }
+# https://www.graalvm.org/latest/reference-manual/ruby/UTF8Locale
+export LANG=en_US.UTF-8
 
-_err_msg() { printf "\033[41m\033[1mError$white %s\n" "$*"; }
+_red() { printf "\033[91m%s\033[0m\n" "$*"; }
+_green() { printf "\033[92m%s\033[0m\n" "$*"; }
+_err_msg() { printf "\033[41m\033[1mError\033[0m %s\n" "$*"; }
+_suc_msg() { printf "\033[42m\033[1mSuccess\033[0m %s\n" "$*"; }
 
 # 各变量默认值
-github_proxy='https://gh-proxy.com/'
+GITHUB_PROXY='https://gh-proxy.com/'
+UA_BROWSER='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/136.0.0.0 Safari/537.36'
 
-os_type=$(uname -s 2>/dev/null | sed 's/[A-Z]/\L&/g')
-os_arch=$(uname -m 2>/dev/null | sed 's/[A-Z]/\L&/g')
-readonly os_type os_arch
+# 用于检查命令是否存在
+_is_exists() {
+    local _CMD="$1"
+    if type "$_CMD" >/dev/null 2>&1; then return 0;
+    elif command -v "$_CMD" >/dev/null 2>&1; then return 0;
+    elif which "$_CMD" >/dev/null 2>&1; then return 0;
+    else return 1;
+    fi
+}
 
 # 清屏函数
 clear_screen() {
-    if [ -t 1 ]; then
-        tput clear 2>/dev/null || echo -e "\033[2J\033[H" || clear
-    fi
+    ( [ -t 1 ] && tput clear 2>/dev/null ) || echo -e "\033[2J\033[H" || clear
 }
 
 pre_check() {
-    if [ "$(id -ru)" -ne 0 ] || [ "$EUID" -ne 0 ]; then
-        _err_msg "$(_red 'This script must be run as root!')" && exit 1
+    if [ "$EUID" -ne 0 ]; then
+        _err_msg "$(_red 'This script must be run as root!')"; exit 1
     fi
     if [ "$(ps -p $$ -o comm=)" != "bash" ] || readlink /proc/$$/exe | grep -q "dash"; then
-        _err_msg "$(_red 'This script needs to be run with bash, not sh!')" && exit 1
+        _err_msg "$(_red 'This script needs to be run with bash, not sh!')"; exit 1
     fi
     if [ "$(cd -P -- "$(dirname -- "$0")" && pwd -P)" != "/root" ]; then
-        cd /root 2>/dev/null || { _err_msg "$(_red 'Failed to change directory, Check permissions and try again!')" && exit 1; }
+        cd /root 2>/dev/null || { _err_msg "$(_red "Failed to change directory, Check permissions and try again!")"; exit 1; }
     fi
-    if [ "$(curl -fskL -m 5 -4 "https://www.qualcomm.cn/cdn-cgi/trace" | grep -i '^loc=' | cut -d'=' -f2 | xargs)" != "CN" ]; then
-        unset github_proxy
+    # 境外服务器仅ipv4访问测试通过后取消github代理
+    if [ "$(curl --user-agent "$UA_BROWSER" -fsL -m 3 --retry 2 -4 "http://www.qualcomm.cn/cdn-cgi/trace" | grep -i '^loc=' | cut -d'=' -f2 | grep .)" != "CN" ]; then
+        unset GITHUB_PROXY
     fi
 }
 
-backtrace_uninstall() {
-    [ -f "$HOME/backtrace" ] && rm -f "$HOME/backtrace" 2>/dev/null
-    [ -f /usr/bin/backtrace ] && rm -f /usr/bin/backtrace 2>/dev/null
-    [ -f /usr/local/bin/backtrace ] && rm -f /usr/local/bin/backtrace 2>/dev/null
+before_run() {
+    [ -f "$HOME/backtrace" ] && rm -f "$HOME/backtrace" >/dev/null 2>&1
+    [ -f /usr/bin/backtrace ] && rm -f /usr/bin/backtrace >/dev/null 2>&1
+    [ -f /usr/local/bin/backtrace ] && rm -f /usr/local/bin/backtrace >/dev/null 2>&1
 }
 
 backtrace_install() {
-    local arch_suffix
+    local OS_TYPE OS_ARCH
+    OS_TYPE="$(uname -s 2>/dev/null | sed 's/[A-Z]/\L&/g')"
 
     # 定义支持的操作系统和架构
-    case "$os_type" in
-        'linux' | 'darwin' | 'freebsd' | 'openbsd') : ;;
-        *) _err_msg "$(_red 'Unsupported operating system:') $(_cyan "$os_type")" && exit 1 ;;
-    esac
-    case "$os_arch" in
-        'x86_64' | 'x86' | 'amd64' | 'x64') arch_suffix="amd64" ;;
-        'i386' | 'i686') arch_suffix="386" ;;
-        'armv7l' | 'armv8' | 'armv8l' | 'aarch64' | 'arm64') arch_suffix="arm64" ;;
-        *) _err_msg "$(_red 'Unsupported architecture:') $(_cyan "$os_arch")" && exit 1 ;;
+    if [ "$OS_TYPE" != linux ] && [ "$OS_TYPE" != darwin ] && [ "$OS_TYPE" != freebsd ] && [ "$OS_TYPE" != openbsd ]; then
+        _err_msg "$(_red "Unsupported operating system: $OS_TYPE")"; exit 1
+    fi
+
+    case "$(uname -m)" in
+        i*86)
+            OS_ARCH="386"
+        ;;
+        x86_64|x86|amd64|x64)
+            OS_ARCH="amd64"
+        ;;
+        armv*|aarch64|arm64)
+            OS_ARCH="arm64"
+        ;;
+        *)
+            _err_msg "$(_red "Unsupported architecture: $(uname -m)")"; exit 1
+        ;;
     esac
 
-    curl -fskL -o backtrace "${github_proxy}https://github.com/oneclickvirt/backtrace/releases/download/output/backtrace-${os_type}-${arch_suffix}" || { _err_msg "$(_red 'Download backtrace failed, Check network and try again!')" && exit 1; }
-    chmod +x backtrace && mv -f backtrace /usr/local/bin/backtrace && backtrace
+    curl -fsL "${GITHUB_PROXY}https://github.com/oneclickvirt/backtrace/releases/download/output/backtrace-${OS_TYPE}-${OS_ARCH}" -o /usr/local/bin/backtrace
+    chmod +x /usr/local/bin/backtrace
+    if _is_exists "backtrace"; then
+        _suc_msg "$(_green "Backtrace Installed Successfully!")"
+    else
+        _err_msg "$(_red "Backtrace Installed fail.")"; exit 1
+    fi
+    clear_screen
 }
 
 backTrace() {
     clear_screen
     pre_check
-    backtrace_uninstall
+    before_run
     backtrace_install
+    backtrace
 }
 
 backTrace
